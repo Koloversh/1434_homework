8b7ed5f7707ec7ef0316081b98605a3dd36f71df
Koloversh <32965666+Koloversh@users.noreply.github.com>
2020-11-16 21:25:57 +0300

    Create Main.java

---
+++ task_5.1/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Дана коллекция имён.
+ * 1) удалить все повторяющиеся имена из коллекции
+ * 2) вывести коллекцию на экран
+ * */
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        ArrayList<String> names = new ArrayList();
+        names.add("Анатолий");
+        names.add("Георгий");
+        names.add("Руслан");
+        names.add("Георгий");
+        names.add("Павел");
+        names.add("Руслан");
+
+        for (int i = 0; i < names.size(); i++) {
+
+            for (int j = i + 1; j < names.size(); j++) {
+                if (names.get(i).equals(names.get(j))) {
+                    names.remove(i);
+                    j--;
+                }
+                
+            }
+        }
+
+        System.out.println(names);
+        
+    }
+}
