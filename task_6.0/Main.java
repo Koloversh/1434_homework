c5c4a60a809baaac93c703b2794fd5d42106c0a8
Koloversh <32965666+Koloversh@users.noreply.github.com>
2020-11-20 16:06:02 +0300

    Third variation

--- task_6.0/Main.java
+++ task_6.0/Main.java
@@ -7,13 +7,13 @@ import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-
-public class ProxyChecker {
+// ____________ЭТО ТРЕТИЙ ВАРИАНТ РЕАЛИЗАЦИИ.
+public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> ipList = new ArrayList<>();
         try {
-            FileReader reader = new FileReader("C://ip.txt");
+            FileReader reader = new FileReader("C://java/ip.txt");//сюда вернул обратно путь к вашему файлу
             int c;
             String proxy = " ";
             while ((c=reader.read()) != -1){
@@ -30,10 +30,6 @@ public class ProxyChecker {
             for (String ipString: ipList) {
                 String ip = ipString.split(":")[0];
                 int port = Integer.parseInt(ipString.split(":")[1]);
-//                Thread thread = new Thread(new MyRunnableClass(ip, port));// способ 2
-//                thread.run();// способ 2
-                
-                //-----Способ 3------------------------------------------
 
                 Thread thread = new Thread(new Runnable() {
                     @Override
@@ -67,70 +63,3 @@ public class ProxyChecker {
         }
     }
 }
-// Способ 2-------------------------------------------------------------
-//class MyRunnableClass implements Runnable {
-//    String ip;
-//    int port;
-//
-//    public MyRunnableClass(String ip,int port){
-//        this.ip = ip;
-//        this.port = port;
-//    }
-//    @Override
-//    public void run(){
-//        System.out.println("Проверяю "+ip+":"+port);
-//        try {
-//            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip,port));
-//            URLConnection connection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
-//            InputStream is = connection.getInputStream();
-//            InputStreamReader reader = new InputStreamReader(is);
-//            char[] buffer = new char[256];
-//            int c;
-//
-//            StringBuilder stringBuilder = new StringBuilder();
-//
-//            while ((c = reader.read(buffer)) != -1){
-//                stringBuilder.append(buffer,0,c);
-//            }
-//            reader.close();
-//            System.out.println(stringBuilder+"РАБОТАЕТ!!!!!!!!!!");
-//
-//        } catch (IOException e) {
-//            System.out.println("ip: "+ip+" не работает");
-//        }
-//    }
-//}
-
-//------Способ 1------------------------------------
-//class MyThread extends Thread{
-//    String ip;
-//    int port;
-//    public MyThread(String ip,int port){
-//        this.ip = ip;
-//        this.port = port;
-//    }
-//    @Override
-//    public void run(){
-//        System.out.println("Проверяю "+ip+":"+port);
-//        try {
-//            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip,port));
-//            URLConnection connection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
-//            InputStream is = connection.getInputStream();
-//            InputStreamReader reader = new InputStreamReader(is);
-//            char[] buffer = new char[256];
-//            int c;
-//
-//
-//            StringBuilder stringBuilder = new StringBuilder();
-//
-//            while ((c = reader.read(buffer)) != -1){
-//                stringBuilder.append(buffer,0,c);
-//            }
-//            reader.close();
-//            System.out.println(stringBuilder+"РАБОТАЕТ!!!!!!!!!!");
-//
-//        } catch (IOException e) {
-//            System.out.println("ip: "+ip+" не работает");
-//        }
-//    }
-//}
